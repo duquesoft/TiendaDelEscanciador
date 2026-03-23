45263cda101189e5bfb2dd45c36c4e589ac79fc5
duquesoft <duquesoft@gmail.com>
2026-03-23 19:53:51 +0100

    Add not-found page to fix static export

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Página no encontrada</h1>
+      <p>La página que buscas no existe.</p>
+    </div>
+  );
+}
